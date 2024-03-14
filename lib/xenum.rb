e8e7112aa28580c997a30024b517c1473a6dbf6a
ken <block24block@gmail.com>
2024-03-14 12:02:19 +0800

    lazy_product

--- lib/xenum.rb
+++ lib/xenum.rb
@@ -3,6 +3,25 @@
 require_relative "xenum/version"
 
 module Xenum
-  class Error < StandardError; end
-  # Your code goes here...
+end
+
+module Enumerable
+  def lazy_product(*enums)
+    if enums.empty?
+      return Enumerator.new do |yielder|
+        each do |this|
+          yielder.yield([this])
+        end
+      end
+    end
+
+    Enumerator.new do |yielder|
+      each do |this|
+        head = [this]
+        enums[0].lazy_product(*enums[1..-1]).each do |tail|
+          yielder.yield(head + tail)
+        end
+      end
+    end
+  end
 end
